b5e72edbcd623f5e69b15e2b77d07ff346f9e823
Pedro Samuel <pedrosviki1265@gmail.com>
2023-04-12 14:12:04 -0400

    OrderStatus

--- src/main/java/com/projetojava/curso/entities/Order.java
+++ src/main/java/com/projetojava/curso/entities/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.projetojava.curso.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -25,6 +26,8 @@ public class Order implements Serializable{
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -33,11 +36,12 @@ public class Order implements Serializable{
 		
 	}
 
-	public Order(Long id, Instant instant, User client) {
+	public Order(Long id, Instant instant, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = instant;
 		this.client = client;
+		this.setOrderStatus(orderStatus);
 	}
 
 	public Long getId() {
@@ -62,6 +66,14 @@ public class Order implements Serializable{
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		this.orderStatus = orderStatus.getCode();
 	}
 
 	@Override
@@ -88,6 +100,5 @@ public class Order implements Serializable{
 			return false;
 		return true;
 	}
-	
 	
 }
